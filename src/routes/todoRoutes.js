b773676f92129454079dc95fe78889680094487a
Runbin Zhou <runbin1000@gmail.com>
2024-12-08 22:47:18 -0500

    changed code for prisma

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -1,42 +1,61 @@
 import express from 'express'
 import db from '../db.js'
+import prisma from '../prismaClient.js'
 
 const router = express.Router()
 
 // Get all todos for logged in user
-router.get('/', (req, res) => {
-    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
-    const todos = getTodos.all(req.userId)
+router.get('/', async (req, res) => {
+    const todos = await prisma.todo.findMany({
+        where: {
+            userId: req.userId
+        }
+    })
     res.json(todos)
 })
 
 // Create a new todo
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const { task } = req.body
-    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
-    const result = insertTodo.run(req.userId, task)
+    const todo = await prisma.todo.create({
+        data:{
+            task,
+            userId: req.userId
+        }
+    })
 
-    res.json({ id: result.lastInsertRowid, task, completed: 0 })
+    res.json(todo)
 })
 
 // Update a todo
-router.put('/:id', (req, res) => { // dynamic id allows us to identity which todo for modifications
+router.put('/:id', async (req, res) => { // dynamic id allows us to identity which todo for modifications
     const { completed } = req.body
     const { id } = req.params
-    const { page } = req.query
 
-    const updatedTodo = db.prepare('UPDATE todos SET completed = ? WHERE id = ?')
-    updatedTodo.run(completed, id)
+    const updatedTodo = await prisma.todo.update({
+        where: {
+            id: parseInt(id),
+            userId: req.userId
+        },
+        data: {
+            completed: !!completed //convert to bool
+        }
+    })
 
-    res.json({ message: "Todo completed" })
+    res.json(updatedTodo)
 })
 
 // Delete a todo
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     const { id } = req.params
     const userId = req.userId
-    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
-    deleteTodo.run(id, userId)
+    
+    const deletedTodo = await prisma.todo.delete({
+        where: {
+            id: parseInt(id),
+            userId
+        }
+    })
     
     res.send({ message: "Todo deleted" })
 })
